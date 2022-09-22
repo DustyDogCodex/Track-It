b323c220988700e4e2cc175f153e89217bacedb9
Varun Malaviya <varunmalaviya7@gmail.com>
2022-09-21 20:19:19 -0500

    had to fix bug in Tasks.js that was preventing tasks from being mapped and loading on screen. Assigned the wrong prop value (text instead of info) from tasks object. Tasks can now be displayed on screen.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
+/* import { useState } from "react"; */
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+
 
 function App() {
+  /* const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        info: "Task #1",
+        time: 'March 8th at 5pm',
+        reminder: true
+    },
+    {
+        id: 2,
+        info: "Task #2",
+        time: 'March 9th at 6pm',
+        reminder: true
+    },
+    {
+        id: 1,
+        info: "Task #1",
+        time: 'March 10th at 9am',
+        reminder: true 
+    }
+  ])
+ */
   return (
     <div className="container">
       <Header />
+      <Tasks /* tasks={tasks} */ />
     </div>
   );
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,34 +1,32 @@
-import { useState } from "react";
+const tasks = [
+    {
+        id: 1,
+        info: "Task #1",
+        time: 'March 8th at 5pm',
+        reminder: true
+    },
+    {
+        id: 2,
+        info: "Task #2",
+        time: 'March 9th at 6pm',
+        reminder: true
+    },
+    {
+        id: 3,
+        info: "Task #1",
+        time: 'March 10th at 9am',
+        reminder: true 
+    }
+]
 
 const Tasks = () => {
-    const [tasks, seTasks] = useState([
-        {
-            id: 1,
-            info: "Task #1",
-            time: 'March 8th at 5pm',
-            reminder: true
-        },
-        {
-            id: 2,
-            info: "Task #2",
-            time: 'March 9th at 6pm',
-            reminder: true
-        },
-        {
-            id: 1,
-            info: "Task #1",
-            time: 'March 10th at 9am',
-            reminder: true 
-        }
-    ])
-
-    return (
-        <>
-            {tasks.map((task) => {
-                <h3 key={task.id}>{task.text}</h3>
-            })}
-        </>
-    )
+  return (
+    <>
+        {tasks.map((task) => (
+            <h3 key={task.id}>{task.info}</h3>
+        ))}
+    </>
+  )
 }
 
 export default Tasks
